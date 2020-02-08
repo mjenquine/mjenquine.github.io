06d972f203ab131eb7decf96c771a351deaf1024
Mark Jenquine <mjenquine@gmail.com>
2020-02-07 19:41:00 -0600

    added modal to splash page and began styling

--- saveRBG_app/app/app.js
+++ saveRBG_app/app/app.js
@@ -1,1 +1,44 @@
-console.log('hi');
+$(() => {
+//=====================================
+  const $openBtn = $('#open-modal-btn');
+  const $modal = $('#modal');
+
+  const openModal = () => {
+    $modal.css('display', 'block');
+  }
+
+  $openBtn.on('click', openModal)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//===================================
+})
