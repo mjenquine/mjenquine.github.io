9a1f0cee405b6fe08a3262011471816c7690368e
Mark Jenquine <mjenquine@gmail.com>
2020-02-08 14:02:15 -0600

    added hide show function to weather modal page

--- saveRBG_app/app/weatherapp.js
+++ saveRBG_app/app/weatherapp.js
@@ -1,3 +1,33 @@
 $(() => {
-  
+  const $title = $('#title')
+  $title.delay(4000).slideUp(1000)
+
+  const $weatherModal = $('#weather-modal')
+
+  const showWeatherModal = () => {
+    $weatherModal.css('display', 'block')
+  }
+  setTimeout(showWeatherModal, 8000);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 })
