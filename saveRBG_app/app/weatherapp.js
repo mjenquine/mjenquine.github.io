d2a5b6a73e1a047a6be544ad683c7f3b792c58a1
Mark Jenquine <mjenquine@gmail.com>
2020-02-08 14:50:08 -0600

    adding ajax promise

--- saveRBG_app/app/weatherapp.js
+++ saveRBG_app/app/weatherapp.js
@@ -1,15 +1,24 @@
 $(() => {
   const $title = $('#title')
   $title.delay(4000).slideUp(1000)
-
   const $weatherModal = $('#weather-modal')
-
   const showWeatherModal = () => {
     $weatherModal.css('display', 'block')
   }
   setTimeout(showWeatherModal, 8000);
 
-
+  $.ajax({
+    url:'https://api.darksky.net/forecast/00dded78a3eaa420a4f554176ecb62db/38.9072,-77.0369',
+    type: "Get"
+  }).then(
+    (data) => {
+      console.log(data);
+    //   $('#currently').html(data)
+    },
+    () => {
+      console.log('no data');
+    }
+  )
 
 
 
