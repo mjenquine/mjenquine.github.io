f307db851e2f4b7edb49fd8f215dedc43ba76e8d
Mark Jenquine <mjenquine@gmail.com>
2020-02-09 12:22:53 -0600

    finished the api

--- html/app/weatherapp.js
+++ html/app/weatherapp.js
@@ -1,11 +1,11 @@
 $(() => {
   const $title = $('#title')
-  $title.delay(4000).slideUp(1000)
+  $title.delay(4000).slideUp(800)
   const $weatherModal = $('#weather-modal')
   const showWeatherModal = () => {
     $weatherModal.css('display', 'block')
   }
-  setTimeout(showWeatherModal, 8000);
+  setTimeout(showWeatherModal, 7000);
 
   $.ajax({
     url: 'http://api.openweathermap.org/data/2.5/weather?lat=38.9072&lon=-77.0369&appid=9720f14bcedd739f2389c4b2d742b802&units=imperial',
@@ -14,10 +14,12 @@ $(() => {
     (data) => {
       $div = $('<div>').html(data.weather[0].description)
       $div.appendTo('#currently')
-      $div = $('<div>').html(data.main.temp)
+
+      $div = $('<div>').html(data.main.temp).addClass('.temp')
       $div.appendTo('#currently')
-      $div = $('<div>').html(data.main.feels_like)
-      $div.appendTo('#currently')
+
+      // $div = $('<div>').html(data.main.feels_like).addClass('.wind-chill')
+      // $div.appendTo('#currently')
     },
     () => {
       console.log('no data');
@@ -26,14 +28,6 @@ $(() => {
 
 
 
-
-
-
-
-
-    // url:'https://api.darksky.net/forecast/00dded78a3eaa420a4f554176ecb62db/38.9072,-77.0369',
-
-// console.log(data.weather[0].description)
 
 
 
