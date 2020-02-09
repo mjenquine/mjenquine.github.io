4ebafde0f8028fd995b1d0b210b6ff8043008b87
Mark Jenquine <mjenquine@gmail.com>
2020-02-08 20:53:13 -0600

    got the api working and appended to the body

--- saveRBG_app/app/weatherapp.js
+++ saveRBG_app/app/weatherapp.js
@@ -8,12 +8,16 @@ $(() => {
   setTimeout(showWeatherModal, 8000);
 
   $.ajax({
-    url:'https://api.darksky.net/forecast/00dded78a3eaa420a4f554176ecb62db/38.9072,-77.0369',
+    url: 'http://api.openweathermap.org/data/2.5/weather?lat=38.9072&lon=-77.0369&appid=9720f14bcedd739f2389c4b2d742b802&units=imperial',
     type: "Get"
   }).then(
     (data) => {
-      console.log(data);
-    //   $('#currently').html(data)
+      $div = $('<div>').html(data.weather[0].description)
+      $div.appendTo('#currently')
+      $div = $('<div>').html(data.main.temp)
+      $div.appendTo('#currently')
+      $div = $('<div>').html(data.main.feels_like)
+      $div.appendTo('#currently')
     },
     () => {
       console.log('no data');
@@ -27,9 +31,9 @@ $(() => {
 
 
 
+    // url:'https://api.darksky.net/forecast/00dded78a3eaa420a4f554176ecb62db/38.9072,-77.0369',
 
-
-
+// console.log(data.weather[0].description)
 
 
 
