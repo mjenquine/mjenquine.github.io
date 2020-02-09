7e7b52b3f5413471cfc068d18241e3f11de9221e
Mark Jenquine <mjenquine@gmail.com>
2020-02-09 13:07:11 -0600

    finished weather button

--- html/app/weatherapp.js
+++ html/app/weatherapp.js
@@ -1,11 +1,19 @@
 $(() => {
   const $title = $('#title')
   $title.delay(4000).slideUp(800)
+
   const $weatherModal = $('#weather-modal')
   const showWeatherModal = () => {
     $weatherModal.css('display', 'block')
   }
+
+  const $doItButton = $('#do-it-button')
+  const showButton = () => {
+    $doItButton.css('display', 'block')
+  }
+
   setTimeout(showWeatherModal, 7000);
+  setTimeout(showButton, 10000);
 
   $.ajax({
     url: 'http://api.openweathermap.org/data/2.5/weather?lat=38.9072&lon=-77.0369&appid=9720f14bcedd739f2389c4b2d742b802&units=imperial',
@@ -17,24 +25,10 @@ $(() => {
 
       $div = $('<div>').html(data.main.temp).addClass('.temp')
       $div.appendTo('#currently')
-
-      // $div = $('<div>').html(data.main.feels_like).addClass('.wind-chill')
-      // $div.appendTo('#currently')
     },
     () => {
       console.log('no data');
     }
   )
-
-
-
-
-
-
-
-
-
-
-
 
 })
