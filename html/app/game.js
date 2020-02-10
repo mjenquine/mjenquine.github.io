1ac12b9b218e615e6d777aa2d1f18e670650b0e0
Mark Jenquine <mjenquine@gmail.com>
2020-02-10 10:40:46 -0600

    added activity squares to game app

--- html/app/game.js
+++ html/app/game.js
@@ -1,1 +1,30 @@
-console.log("hiiiii");
+//activities array
+const arr = ['Do Pilates', 'Eat Kale', 'Take Vitamins', 'Take a walk', 'Hug a Friend', 'Meditate']
+const hp = 
+
+$(() => {
+//===================================
+
+
+const generateActivities = () => {
+  for (let i = 0; i < arr.length; i++) {
+    //console.log(arr[i]);
+    const $activity = $(`<div> ${arr[i]}</div>`).addClass('activity-squares')
+    $('#squares-section').append($activity)
+
+
+
+  }
+}
+
+
+
+
+
+
+
+
+
+generateActivities()
+//=====================================
+})
