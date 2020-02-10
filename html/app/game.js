8687eb4afb1b42f60a47f272c7b02816743b3871
Mark Jenquine <mjenquine@gmail.com>
2020-02-10 12:18:17 -0600

    added total health to game.html

--- html/app/game.js
+++ html/app/game.js
@@ -1,6 +1,7 @@
 //activities array
 const arr = ['Do Pilates', 'Eat Kale', 'Take Vitamins', 'Take a walk', 'Hug a Friend', 'Meditate']
 
+const totalHP = 0
 
 $(() => {
 //===================================
@@ -11,18 +12,18 @@ const generateActivities = () => {
     //create a random health point between 0 and 20
     const hp = [Math.floor(Math.random() * 20)]
     //these are the main activity buttons appended to the .squares-section
-    const $activity = $(`<div> ${arr[i]}</div>`).addClass('activity-squares')
+    const $activity = $(`<div> ${arr[i]}</div>`).addClass('activity-squares');
     $('#squares-section').append($activity)
     //this appends the random health points to the activity buttons
-    const $hp = $(`<div>${hp[0]}</div>`).addClass('activity-hp')
-    console.log($hp);
+    const $hp = $(`<div>${hp[0]}</div>`).addClass('activity-hp');
     ($activity).append($hp)
-
-
-
   }
 }
-
+  //captures totalHP and displays it on the screen
+  const displayTotalHP = () => {
+    const $health = $(`<div>${totalHP}</div>`)
+    $('#total-score-box').append($health)
+  }
 
 
 
@@ -32,5 +33,6 @@ const generateActivities = () => {
 
 
 generateActivities()
+displayTotalHP()
 //=====================================
 })
