5a868892c7b049a4d51e5a14e2a0c7b9649f3a5e
Mark Jenquine <mjenquine@gmail.com>
2020-02-10 15:08:55 -0600

    refactoring gernerateActivities

--- html/app/game.js
+++ html/app/game.js
@@ -5,18 +5,50 @@ const totalHP = 0
 
 $(() => {
 //===================================
+const generateActivities = () => {
+  for (let i = 0; i < arr.length; i++) {
+
+    //these are the main activity buttons appended to the .squares-section
+    const $activity = $(`<div> ${arr[i]}</div>`).addClass('activity-squares');
+    $('#squares-section').append($activity)
+    //this appends the random health points to the activity buttons
+    const $hp = $(`<div>${hp}</div>`).addClass('activity-hp');
+    ($activity).append($hp)
+    //add an event listener to the activity squares
+    $activity.on('click', (event) => {
+      let newHealth = parseInt($activity.children().html())
+      console.log(newHealth);
+      //create a random health point between 0 and 20
+      const hp = Math.floor(Math.random() * 20)
+    })
+  }
+}
+
+
+
+
+
+
+
+
 
 
 const generateActivities = () => {
   for (let i = 0; i < arr.length; i++) {
     //create a random health point between 0 and 20
-    const hp = [Math.floor(Math.random() * 20)]
+    const hp = Math.floor(Math.random() * 20)
     //these are the main activity buttons appended to the .squares-section
     const $activity = $(`<div> ${arr[i]}</div>`).addClass('activity-squares');
     $('#squares-section').append($activity)
     //this appends the random health points to the activity buttons
-    const $hp = $(`<div>${hp[0]}</div>`).addClass('activity-hp');
+    const $hp = $(`<div>${hp}</div>`).addClass('activity-hp');
     ($activity).append($hp)
+    //add an event listener to the activity squares
+    $activity.on('click', (event) => {
+      let newHealth = parseInt($activity.children().html())
+      console.log(newHealth);
+
+    })
   }
 }
   //captures totalHP and displays it on the screen
@@ -25,9 +57,7 @@ const generateActivities = () => {
     $('#total-score-box').append($health)
   }
 
-  $('.activity-squares').on('click', (event) => {
-    event.console.log('hiiiii');
-  })
+
 
 
 
