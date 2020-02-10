31512d3b7d9f006711b49cecf9b58bc4b7b55205
Mark Jenquine <mjenquine@gmail.com>
2020-02-10 11:06:56 -0600

    added hp to the activities

--- html/app/game.js
+++ html/app/game.js
@@ -1,6 +1,6 @@
 //activities array
 const arr = ['Do Pilates', 'Eat Kale', 'Take Vitamins', 'Take a walk', 'Hug a Friend', 'Meditate']
-const hp = 
+
 
 $(() => {
 //===================================
@@ -8,9 +8,15 @@ $(() => {
 
 const generateActivities = () => {
   for (let i = 0; i < arr.length; i++) {
-    //console.log(arr[i]);
+    //create a random health point between 0 and 20
+    const hp = [Math.floor(Math.random() * 20)]
+    //these are the main activity buttons appended to the .squares-section
     const $activity = $(`<div> ${arr[i]}</div>`).addClass('activity-squares')
     $('#squares-section').append($activity)
+    //this appends the random health points to the activity buttons
+    const $hp = $(`<div>${hp[0]}</div>`).addClass('activity-hp')
+    console.log($hp);
+    ($activity).append($hp)
 
 
 
